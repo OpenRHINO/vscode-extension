f2f77ffba0d3d0efe639b31a2127ea1e31d4b635
LI-Mingyu <limingyu@ict.ac.cn>
2023-06-06 14:21:50 +0800

    POC implementation of showing logs of rhinojobs in Webview

--- src/extension.ts
+++ src/extension.ts
@@ -4,10 +4,10 @@ import * as vscode from 'vscode';
 
 import rhinoCreate from './cmd/create';
 import rhinoBuild from './cmd/build';
-import rhinoLogs from './cmd/logs';
 import rhinoRun from './cmd/run';
 import { JobTreeItem, RhinoJobsProvider, refreshRhinoJobList, deleteRhinoJob } from './provider/rhinojob_provider';
 import RhinoGPTProvider from './provider/gpt_provider';
+import { execSync } from 'child_process';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -22,7 +22,7 @@ export function activate(context: vscode.ExtensionContext) {
 		vscode.commands.registerCommand('rhino.create', rhinoCreate),
 		vscode.commands.registerCommand('rhino.build', () => rhinoBuild(outputChannel)),
 		vscode.commands.registerCommand('rhino.run', rhinoRun),
-		vscode.commands.registerCommand('rhino.logs',()=>rhinoLogs(logsOutputChannel)),
+		vscode.commands.registerCommand('rhino.logs', (jobItem: JobTreeItem) => rhinoLogs(logsOutputChannel, jobItem.job?.name)),
 		vscode.commands.registerCommand('rhino.jobs.refresh', () => refreshRhinoJobList(rhinoJobsProvider)),
 		vscode.commands.registerCommand('rhino.jobs.delete', (jobItem: JobTreeItem) => {
 			deleteRhinoJob(rhinoJobsProvider, jobItem.job?.name)
@@ -31,6 +31,35 @@ export function activate(context: vscode.ExtensionContext) {
 
 	refreshRhinoJobList(rhinoJobsProvider)
 }
+
+function rhinoLogs(outputChannel: vscode.OutputChannel, jobName: string | undefined) {
+	if (!jobName) {
+		outputChannel.appendLine('Error: no job name provided');
+		return;
+	}
+
+	const logsOutput = execSync(`rhino logs ${jobName}`).toString();
+
+	const panel = vscode.window.createWebviewPanel(
+		'rhinoLogs', // Identifies the type of the webview. Used internally
+		`Logs for job ${jobName}`, // Title of the panel displayed to the user
+		vscode.ViewColumn.One, // Editor column to show the new webview panel in.
+		{} // Webview options. More on these later.
+	);
+
+	panel.webview.html = `
+  <body>
+    <pre>${logsOutput}</pre>
+    <script>
+      window.addEventListener('load', (event) => {
+        window.scrollTo(0, document.body.scrollHeight);
+      });
+    </script>
+  </body>`;
+
+
+}
+
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
